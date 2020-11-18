14f191c9264addd3b667f9b645edca1b7c192148
namansukhwani <59639173+namansukhwani@users.noreply.github.com>
2020-11-18 14:42:52 +0530

    Delete 1_binary_to_no.py

--- 1_binary_to_no.py
+++
@@ -1,9 +0,0 @@
-bi = input("Enter a binary number: ")
-n = int(bi, 2)
-print("Before addition of 1 in decimal: ", n)
-print("Before addition of 1 in binary: ", bi)
-n += 1
-bi = bin(n).replace("0b", "")
-n = int(bi, 2)
-print("After addition of 1 in decimal: ", n)
-print("After addition of 1 in binary: ", bi)
